31886cd0432e9bff63785fc9789fe286a2a31caf
Anjali Gupta <anjaligupta0621@gmail.com>
2023-04-20 00:08:39 -0400

    minor improvements

--- frontend/src/Signup.jsx
+++ frontend/src/Signup.jsx
@@ -9,6 +9,7 @@ function Signup() {
 
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [retypePassword, setRetypePassword] = useState('');
 
     const onChangeUsername = (e) => {
         const username = e.target.value;
@@ -18,6 +19,11 @@ function Signup() {
     const onChangePassword = (e) => {
         const password = e.target.value;
         setPassword(password);
+    }
+
+    const onChangeRetypePassword = (e) => {
+        const retypePassword = e.target.value;
+        setRetypePassword(retypePassword);
     }
 
     const onSubmit = () => {
@@ -33,7 +39,8 @@ function Signup() {
         justifyContent="center"
         style={{ minHeight: '100vh' }}
         >
-        <Grid item xs={6}>
+        <Grid item xs={6} width={'100vw'}
+        height={'100vh'}>
                
     <MDBContainer fluid className="p-3 my-5 h-custom">
 
@@ -43,7 +50,7 @@ function Signup() {
           <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp" class="img-fluid" alt="Sample image" />
         </MDBCol>
 
-        <MDBCol col='4' md='6'>
+        <MDBCol col='4' md='4'>
 
           <div className="divider d-flex align-items-center my-4">
             <p className="text-center fw-bold mx-3 mb-0">Sign Up</p>
@@ -51,7 +58,7 @@ function Signup() {
 
           <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg" onChange={onChangeUsername}/>
           <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg" onChange={onChangePassword} />
-          <MDBInput wrapperClass='mb-4' label='Re-Type Password' id='formControlLg' type='password' size="lg" onChange={onChangePassword} />
+          <MDBInput wrapperClass='mb-4' label='Re-Type Password' id='formControlLg' type='password' size="lg" onChange={onChangeRetypePassword} />
 
 
           <div className='text-center text-md-start mt-4 pt-2'>
