e473947b7c88223c928728c282579cc4b06665b2
Mikky <meekyberry6@gmail.com>
2023-04-21 13:41:33 +0100

    fix: Removed localStorage & add state 4 empty task

---
+++ .history/frontend/src/components/tasks/taskLists_20230421115651.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Task from "./task";
+import "../../styles/taskList.css";
+
+const TaskList = ({ tasks, onDelete, onEdit }) => {
+  return (
+    <div className="taskList">
+      <h1 className="taskList--head">Backlog</h1>
+      {tasks.map((task, index) => (
+        <Task
+          key={index}
+          index={index}
+          task={task}
+          onDelete={onDelete}
+          onEdit={onEdit}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default TaskList;
